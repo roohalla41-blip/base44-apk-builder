c661dd05b90cfdbd82f2c53f2ecf717be73cc902
roohalla41-blip <roohalla41@gmail.com>
2026-09-20 15:40:50 +0330

    Create MainActivity.java
    Add MainActivity

---
+++ app/src/main/java/com/base44/apk/MainActivity.java
@@ -0,0 +1,40 @@
+package com.base44.apk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setSupportZoom(false);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://YOUR-BASE44-APP.base44.app");
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
